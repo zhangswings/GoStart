e33a1cdf3154fea70359da1e17a008ed93e29f13
张庆 <qing.zhang@shandian.net>
2020-05-10 23:39:51 +0800

    Test

--- day01/helloword/main.go
+++ day01/helloword/main.go
@@ -1,6 +1,26 @@
 package main
 
 import "fmt"
-func main()  {
-	fmt.Println("Hello world.")
+
+// func main()  {
+// 	fmt.Println("Hello world.")
+// 	fmt.Println("Hello Hello")
+// }
+
+// 批量声明
+var (
+	name string //" "
+	age  int    //0
+	isOK bool   //false
+)
+
+func main() {
+	name = "lixiang"
+	age = 16
+	isOK = true
+
+	fmt.Println(isOK)
+	fmt.Printf("name: %s\n", name)
+	fmt.Println(age)
+
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module goStart/day01
+
+go 1.13
