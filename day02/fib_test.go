6ec3a4e3ef178f9721a8db4b74c397dda228b432
zhangswings <beforenight@163.com>
2020-05-19 14:38:06 +0800

    fib切片

---
+++ day02/fib_test.go
@@ -0,0 +1,29 @@
+package fib
+
+import (
+	"testing"
+)
+
+func TestFibList(t *testing.T) {
+
+	a := 1
+	b := 1
+
+	t.Log(a)
+
+	for i := 0; i < 5; i++ {
+		t.Log(" ", b)
+		tmp := a
+		a = b
+		b = tmp + a
+	}
+
+}
+
+func TestExchange(t *testing.T) {
+	a := 1
+	b := 2
+
+	a, b = b, a
+	t.Log(a, b)
+}
