851daa312339dc6188b55bcba6616ae068c2e1e3
zhangswings <beforenight@163.com>
2020-05-21 14:21:05 +0800

    slice can only be compared to nil

--- day03/slice_test.go
+++ day03/slice_test.go
@@ -26,3 +26,24 @@ func TestSliceShareMemory(t *testing.T) {
 
 	t.Log(Q2, len(Q2), cap(Q2))
 }
+
+/**
+数组 VS. 切片
+1. 容量是否可伸缩
+2. 是否可以进行比较
+*/
+func TestSliceComparing(t *testing.T) {
+	a := []int{1, 2, 3, 4}
+	//b:=[]int{1,2,3,4}
+
+	//Error: invalid operation: a == b (slice can only be compared to nil)
+	//if a == b {
+	//	t.Log("Equal")
+	//}
+
+	if a == nil {
+		t.Log("Nil")
+	} else {
+		t.Log("Not Nil")
+	}
+}
