b07b9003f0e6d5e86d2513f5060105b26e452a40
zhangswings <beforenight@163.com>
2020-05-28 11:38:09 +0800

    new与make的区别

--- day12/go_new_test.go
+++ day12/go_new_test.go
@@ -40,3 +40,19 @@ func TestNewFunc(t *testing.T) {
 	fmt.Println(b)
 	fmt.Println(*b)
 }
+
+//make也是用于内存分配的，区别于new，它只用于slice、map以及chan的内存创建，
+//而且它返回的类型就是这三个类型本身，而不是他们的指针类型，
+//因为这三种类型就是引用类型，所以就没有必要返回他们的指针了。
+//func make(t Type, size ...IntegerType) Type
+func TestMakeFunc(t *testing.T) {
+	var b map[string]int
+	b = make(map[string]int, 10)
+	b["A"] = 100
+	fmt.Println(b)
+}
+
+//new与make的区别
+//二者都是用来做内存分配的。
+//make只用于slice、map以及channel的初始化，返回的还是这三个引用类型本身；
+//而new用于类型的内存分配，并且内存对应的值为类型零值，返回的是指向类型的指针。
